e26f64efcc52aac16fe28f266a7cc5e306fcb2e8
Aleksey Gushchin <119592787+AleksGushch@users.noreply.github.com>
2022-12-17 23:01:08 +0300

    Add files via upload

--- HP.cs
+++ HP.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class HP : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
-    [SerializeField] private float timerForHit;
     private float currentHealth;
     private float hpForInterface;
     private float currentHitTimer;
@@ -28,20 +27,23 @@ public class HP : MonoBehaviour
 
     private void Update()
     {
-        if (currentHealth > maxHealth)
-            currentHealth = maxHealth;
-
-        hpForInterface = currentHealth / maxHealth;
-
+        IsHit();
+        SelfDestroy();
         if (IsAlive == false && !gameObject.CompareTag("Player")) 
         {
-
-            Destroy(gameObject);
+            ClearChildren();
+            Destroy(gameObject, 1f);
         }
+        if (currentHealth > maxHealth)//ХП не может быть больше максимума
+            currentHealth = maxHealth;
+        hpForInterface = currentHealth / maxHealth;
+    }
 
+    private void IsHit() 
+    {
         if (isDamage)
         {
-            if (currentHitTimer <= timerForHit)
+            if (currentHitTimer <= 0.3f)
             {
                 currentHitTimer += Time.deltaTime;
             }
@@ -50,6 +52,30 @@ public class HP : MonoBehaviour
                 currentHitTimer = 0;
                 isDamage = false;
             }
+        }
+    }
+
+    private void ClearChildren() 
+    {
+        GameObject[] allChildrenObjects = new GameObject[transform.childCount];
+        int i = 0;
+        foreach (Transform child in transform) 
+        {
+            allChildrenObjects[i] = child.gameObject;
+            i += 1;
+        }
+        foreach (GameObject child in allChildrenObjects) 
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void SelfDestroy()
+    {
+        if (transform.childCount < 1)
+        {
+            TakeDamage(1000);//перед установкой значения сравни значение урона в дочернем объекте.
+            Destroy(gameObject, 1f);
         }
     }
 }
